4aadc8ad25494b6217bc4bad63c0f81cf33028cc
Maria <mariavarava176@gmail.com>
2024-10-07 18:36:27 +0300

    Added controller for user login

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const JDentIcon = require('jdenticon');
 const path = require('path');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -40,12 +41,35 @@ const UserController = {
 
       res.status(201).json(userResponse);
     } catch (error) {
-      console.error('Error in register:', error);
+      console.error('Error in register: ', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
   login: async (req, res) => {
-    res.send('login');
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (!existingUser) {
+        return res.status(400).json({ error: 'Ivalid email or password' });
+      }
+
+      const isValid = await bcrypt.compare(password, existingUser.password);
+      if (!isValid) {
+        return res.status(400).json({ error: 'Ivalid email or password' });
+      }
+
+      const token = jwt.sign({ userId: existingUser._id }, process.env.SECRET_KEY);
+
+      res.json({ token });
+    } catch (error) {
+      console.error('Error in login: ', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   },
   getUserById: async (req, res) => {
     res.send('getUserById');
