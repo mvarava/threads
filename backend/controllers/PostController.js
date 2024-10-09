85119135fa4956856701eda15de0101ce113dd8f
Maria <mariavarava176@gmail.com>
2024-10-09 10:50:54 +0300

    Added controller deleting post

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -1,8 +1,9 @@
-const bcrypt = require('bcryptjs');
-const JDentIcon = require('jdenticon');
-const path = require('path');
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+// const JDentIcon = require('jdenticon');
+// const path = require('path');
+// const fs = require('fs');
+// const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const User = require('../models/User');
 const Post = require('../models/Post');
@@ -85,7 +86,7 @@ const PostController = {
         });
 
       if (!wantedPost) {
-        return res.status(404).json({ error: 'No post found' });
+        return res.status(404).json({ error: 'Such post was not found' });
       }
 
       const postWithLikeInfo = {
@@ -100,7 +101,38 @@ const PostController = {
     }
   },
   deletePost: async (req, res) => {
-    res.send('deletePost');
+    const { id } = req.params;
+    const userId = req.user.userId;
+
+    const postToDelete = await Post.findById(id);
+
+    if (!postToDelete) {
+      return res.status(404).json({ error: 'Such post was not found' });
+    }
+
+    console.log(postToDelete.author);
+    if (!postToDelete.author.equals(userId)) {
+      return res.status(403).json({ error: 'No permission' });
+    }
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      await Comment.deleteMany({ post: id }).session(session);
+
+      await Like.deleteMany({ post: id }).session(session);
+
+      await Post.deleteOne({ _id: id }).session(session);
+
+      await session.commitTransaction();
+      session.endSession();
+
+      res.status(200).json({ message: 'Post and related data deleted successfully' });
+    } catch (error) {
+      console.error('Error in deletePost: ', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   },
 };
 
